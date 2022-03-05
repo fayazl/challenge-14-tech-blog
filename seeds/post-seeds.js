6427e974687f85f7569217031e0c0d3688982642
Fayaz <flalani100@gmail.com>
2022-03-05 01:08:53 -0500

    Modified seeds data

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,42 +3,42 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'Donec posuere metus vitae ipsum.',
-    post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
-    user_id: 10
+    description: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    user_id: 1
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'https://nasa.gov/donec.json',
-    user_id: 8
+    description: 'https://nasa.gov/donec.json',
+    user_id: 2
   },
   {
     title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    description: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
     user_id: 1
   },
   {
     title: 'Nunc purus.',
-    post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
+    description: 'http://desdev.cn/enim/blandit/mi.jpg',
     user_id: 4
   },
   {
     title: 'Pellentesque eget nunc.',
-    post_url: 'http://google.ca/nam/nulla/integer.aspx',
-    user_id: 7
+    description: 'http://google.ca/nam/nulla/integer.aspx',
+    user_id: 5
   },
   {
     title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    post_url: 'https://stanford.edu/consequat.png',
-    user_id: 4
+    description: 'https://stanford.edu/consequat.png',
+    user_id: 5
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
-    user_id: 1
+    description: 'http://edublogs.org/non/ligula/pellentesque.js',
+    user_id: 3
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
+    description: 'http://ucla.edu/consequat/nulla.html',
     user_id: 1
   }
 ];
