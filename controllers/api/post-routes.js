61ce4856edb32da6a3746b8f015ed407d9513385
Fayaz <flalani100@gmail.com>
2022-03-05 01:01:24 -0500

    Added handlebars

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -75,7 +75,6 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-  // expects {title: 'Taskmaster goes public!', description: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     description: req.body.description,
